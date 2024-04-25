af32f923049c64999d24b909722670dae2ddf98b
Brendan Benlolo <bbenlo1@students.towson.edu>
2024-04-25 18:10:22 -0400

    Added styling sheet and small refactoring

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,9 @@
+const linkElement = document.createElement('link');
+linkElement.rel = 'stylesheet';
+linkElement.type = 'text/css';
+linkElement.href = chrome.runtime.getURL('styles/styles.css');
+document.head.appendChild(linkElement);
+
 let definitionsBox; // Declare a variable to hold the reference to the definitions box
 let selectedDefinitionsBox;
 let selectionRect;
@@ -25,7 +31,7 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
           entry.meanings.forEach((meaning) => {
             let partOfSpeech = "(" + meaning.partOfSpeech + ") ";
             meaning.definitions.forEach((definition) => {
-              definitionsBox.appendChild(createDefinitionElement(definition, partOfSpeech));
+              definitionsBox.appendChild(createDefinition(definition.definition, partOfSpeech));
             });
           });
         });
@@ -39,58 +45,30 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 function createDefinitionsBox() {
   selectionRect = window.getSelection().getRangeAt(0).getBoundingClientRect();
   definitionsBox = document.createElement("div");
-  definitionsBox.id = "definitions-box";
 
-  definitionsBox.style.position = "absolute";
-  definitionsBox.style.backgroundColor = "white";
-  definitionsBox.style.border = "1px solid black";
-  definitionsBox.style.padding = "5px";
+  definitionsBox.classList.add("definitionsBox");
   definitionsBox.style.top = selectionRect.bottom + window.scrollY + "px";
   definitionsBox.style.left = selectionRect.left + "px";
-  definitionsBox.style.zIndex = "9999";
-  definitionsBox.style.height = "150px";
-  definitionsBox.style.overflowY = "auto";
-  definitionsBox.style.borderRadius = "8px";
 
   document.body.appendChild(definitionsBox);
 }
 
-function createDefinitionElement(definition, partOfSpeech) {
-  const definitionElement = document.createElement("p");
-  definitionElement.textContent = partOfSpeech + definition.definition;
+function createDefinition(definitionText, partOfSpeech) {
+  const definition = document.createElement("p");
+  definition.textContent = partOfSpeech + definitionText;
 
-  definitionElement.style.color = "black";
-  definitionElement.style.fontFamily = "Arial, sans-serif";
-  definitionElement.style.fontSize = "14px";
-  definitionElement.style.margin = "0";
-  definitionElement.style.padding = "2px 2px 2px 5px";
+  definition.classList.add("definition");
 
-  definitionElement.addEventListener("mouseenter", function () {
-    definitionElement.style.backgroundColor = "#f0f0f0";
-  });
-  definitionElement.addEventListener("mouseleave", function () {
-    definitionElement.style.backgroundColor = "transparent";
+  definition.addEventListener("click", function () {
+    createSpan(definition.textContent);
   });
 
-  definitionElement.addEventListener("click", function () {
-    createSpan(definitionElement.textContent);
-  });
-
-  return definitionElement;
+  return definition;
 }
 
 function createSpan(definition) {
   const span = document.createElement("span");
-  span.style.fontWeight = "bold";
-  span.style.color = "royalblue";
-  span.style.fontStyle = "italic";
-
-  span.addEventListener("mouseover", function () {
-    span.style.cursor = "pointer"; // Change cursor to pointer when hovering
-  });
-  span.addEventListener("mouseout", function () {
-    span.style.cursor = "default"; // Revert cursor to default when not hovering
-  });
+  span.classList.add("definedWord");
 
   span.addEventListener("click", function () {
     createSelectedDefinitionsBox(span, definition);
@@ -108,36 +86,20 @@ function createSelectedDefinitionsBox(span, definition) {
     const spanRect = span.getBoundingClientRect();
     selectedDefinitionsBox = document.createElement("div");
     
-    selectedDefinitionsBox.style.position = "absolute";
-    selectedDefinitionsBox.style.backgroundColor = "white";
-    selectedDefinitionsBox.style.border = "1px solid black";
-    selectedDefinitionsBox.style.padding = "5px";
+    selectedDefinitionsBox.classList.add("definitionsBox");
     selectedDefinitionsBox.style.top = spanRect.bottom + window.scrollY + "px";
     selectedDefinitionsBox.style.left = spanRect.left + "px";
-    selectedDefinitionsBox.style.zIndex = "9999";
-    selectedDefinitionsBox.style.overflowY = "auto";
-    selectedDefinitionsBox.style.borderRadius = "8px";
 
-    const selectedDefinitionElement = document.createElement("p");
-    selectedDefinitionElement.textContent = definition;
-    selectedDefinitionElement.style.color = "black";
-    selectedDefinitionElement.style.fontFamily = "Arial, sans-serif";
-    selectedDefinitionElement.style.fontSize = "14px";
-    selectedDefinitionElement.style.margin = "0";
-    selectedDefinitionElement.style.padding = "2px 2px 2px 5px";
-    // Add hover effect
-    selectedDefinitionElement.addEventListener("mouseenter", function () {
-      selectedDefinitionElement.style.backgroundColor = "#f0f0f0";
-    });
-    selectedDefinitionElement.addEventListener("mouseleave", function () {
-      selectedDefinitionElement.style.backgroundColor = "transparent";
-    });
-    selectedDefinitionElement.addEventListener("click", function () {
+    const selectedDefinition = document.createElement("p");
+    selectedDefinition.textContent = definition;
+    selectedDefinition.classList.add("definition");
+
+    selectedDefinition.addEventListener("click", function () {
       selectedDefinitionsBox.remove();
       selectedDefinitionsBox = null;
     });
     document.body.appendChild(selectedDefinitionsBox);
-    selectedDefinitionsBox.appendChild(selectedDefinitionElement);
+    selectedDefinitionsBox.appendChild(selectedDefinition);
   }
 }
 
